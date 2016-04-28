32fd3e43ca7349c7625eb1e2d88ba3c11b521c56
Ethan Long <ethan.long@vertical-knowledge.com>
2016-04-28 00:28:51 -0400

    get channel id

--- slack_bot.js
+++ slack_bot.js
@@ -95,26 +95,30 @@ controller.hears('!contractup', 'ambient', function(bot, message) {
     })
 });
 
-// controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-//     bot.api.reactions.add({
-//         timestamp: message.ts,
-//         channel: message.channel,
-//         name: 'robot_face',
-//     }, function(err, res) {
-//         if (err) {
-//             bot.botkit.log('Failed to add emoji reaction :(', err);
-//         }
-//     });
+controller.hears(['testing channel'], 'direct_message,direct_mention,mention', function(bot, message) {
+    bot.say({
+        text: 'testing ' + message.channel,
+        channel: message.channel
+    })
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
 
 
-//     controller.storage.users.get(message.user, function(err, user) {
-//         if (user && user.name) {
-//             bot.reply(message, 'Hello ' + user.name + '!!');
-//         } else {
-//             bot.reply(message, 'Hello.');
-//         }
-//     });
-// });
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello.');
+        }
+    });
+});
 
 // controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
 //     var name = message.match[1];

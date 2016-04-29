d28d515a59c2da94ba5e8952cccdf7d45c8647ef
Ethan Long <ethan.long@vertical-knowledge.com>
2016-04-29 15:35:58 -0400

    added @channel

--- slack_bot.js
+++ slack_bot.js
@@ -90,7 +90,7 @@ var bot = controller.spawn({
 //
 controller.hears('!contractup', 'ambient', function(bot, message) {
     bot.say({
-        text: 'Contract up ' + message.text.substr(message.text.indexOf('!contractup ') + 12),
+        text: '@channel Contract up ' + message.text.substr(message.text.indexOf('!contractup ') + 12),
         channel: 'G100P3J9E'
     })
 });
